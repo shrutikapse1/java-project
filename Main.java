b275093c628e6bc5c82b7a436816aff339051056
shruti kapse <81238358+shrutikapse1@users.noreply.github.com>
2024-03-04 10:46:28 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package java_project;
 
 import java.util.Date;
 
-public class Main {
+public class Demo {
 	public static void main(String[] args) {
 		System.out.println("This is java project");
 		System.out.println("output from java program");
