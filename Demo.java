1e1726798a9b27a32e8b0b140942724b29989855
shruti kapse <81238358+shrutikapse1@users.noreply.github.com>
2024-03-04 10:51:37 +0530

    Demo push

---
+++ Demo.java
@@ -0,0 +1,12 @@
+package java_project;
+
+import java.util.Date;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("This is java project");
+		System.out.println("output from java program");
+		System.out.println(new Date());
+	}
+
+}
